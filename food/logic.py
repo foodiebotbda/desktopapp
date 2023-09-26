85746e7d5c88919998b3ab726a36d3c5ded44e47
Studentalthaf <09010621013@student.uinsby.ac.id>
2023-09-26 12:00:08 +0700

    perubahandari sqlite-MAriaDB

--- food/logic.py
+++ food/logic.py
@@ -12,27 +12,7 @@ class FoodAppBackend:
             database="FoodAppDatabase"
         )
         self.cursor = self.conn.cursor()
-    # def save_makanan(self, nama, tempat, gambar, harga):
-    #     try:
-    #         self.cursor.execute("INSERT INTO Makanan (nama_makanan, tempat, gambar, harga) VALUES (?, ?, ?, ?)",
-    #                             (nama, tempat, gambar, harga))
-    #         self.conn.commit()  # Simpan perubahan ke database
-    #         return True
-    #     except Exception as e:
-    #         print("Error:", str(e))
-    #         return False
 
-    # def save_kategori(self, id_makanan, deskripsi_makanan_list):
-    #     try:
-    #         # Mengonversi list deskripsi menjadi format JSON
-    #         deskripsi_makanan_json = json.dumps(deskripsi_makanan_list)
-    #         self.cursor.execute("INSERT INTO Kategori (id_makanan, deskripsi_makanan) VALUES (?, ?)",
-    #                             (id_makanan, deskripsi_makanan_json))
-    #         self.conn.commit()  # Simpan perubahan ke database
-    #         return True
-    #     except Exception as e:
-    #         print("Error saat menyimpan data kategori:", str(e))
-    #         return False
     def save_makanan(self, nama, tempat, gambar, harga):
         try:
             self.cursor.execute("INSERT INTO Makanan (nama_makanan, tempat, gambar, harga) VALUES (%s, %s, %s, %s)",
